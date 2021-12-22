0f4ceca20308fbeb3afbfab941bd6e7831a9bf17
tugsag <tg5849@gmail.com>
2021-12-22 11:26:25 -0600

    license

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Very simple program to generate random strings of either characters or integers of given length and run an experiment to count how many palindromes on avergae there are. Created mainly to compare C++ performance against Python on large experiment sizes and loop efficiencies. Python requires **matplotlib** and **numpy**. C++ has no special requirements. 
+
+For the **check.cpp** file, there is also a license plate simulator to check for palindromes in US license plates of the form "char char char int int int int". 
--- check.cpp
+++ check.cpp
@@ -121,14 +121,44 @@ vector<char> gen_str(int n){
     return a;
 }
 
-int main(){
-    srand(time(0));
-    auto start = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    int epochs = 15;
-    int runs = 200;
-    int n = 250;
-    // char palindromes
-    cout << "-------Char Palindromes-------" << "\n";
+int license(int epochs, int runs){
+    // XXX 0000
+    cout << "\n" << "---------License Sim---------" << "\n";
+    vector<double> res(epochs);
+    for(int e = 0; e < epochs; e++){
+        int run_sum = 0;
+        for(int r = 0; r < runs; r++){
+            int npc = check_pal_char(gen_str(3));
+            int npi = check_pal(gen_arr(4));
+            run_sum += npc + npi;
+        }
+        res[e] = run_sum /(double) runs;
+    }
+
+    for(auto i: res){
+        cout << setprecision(5) << i << " ";
+    }
+    return 1;
+}
+
+int arr_exp(int epochs, int runs, int n){
+    cout << "\n" << "---------Int Palindromes----------" << "\n";
+    vector<double> res(epochs);
+    for(int e = 0; e < epochs; e++){
+        int run_sum = 0;
+        for(int r = 0; r < runs; r++){
+            run_sum += check_pal(gen_arr(n));
+        }
+        res[e] = run_sum /(double) runs;
+    }
+    for(auto i: res){
+        cout << setprecision(10) << i << " ";
+    }
+    return 1;
+}
+
+int str_exp(int epochs, int runs, int n){
+    cout << "\n" << "-------Char Palindromes-------" << "\n";
     vector<double> res(epochs);
     for(int e = 0; e < epochs; e++){
         int run_sum = 0;
@@ -140,22 +170,19 @@ int main(){
     for(auto i: res){
         cout << setprecision(10) << i << " ";
     }
-    // auto end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    // cout << "\n" << setprecision(10) << (end -(double) start) /(double) 1000 << "\n";
+    return 1;
+}
 
-    // int palindromes
-    // vector<double> res(epochs);
-    cout << "\n" << "---------Int Palindromes----------" << "\n";
-    for(int e = 0; e < epochs; e++){
-        int run_sum = 0;
-        for(int r = 0; r < runs; r++){
-            run_sum += check_pal(gen_arr(n));
-        }
-        res[e] = run_sum /(double) runs;
-    }
-    for(auto i: res){
-        cout << setprecision(10) << i << " ";
-    }
+int main(){
+    srand(time(0));
+    auto start = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    int epochs = 15;
+    int runs = 200;
+    int n = 250;
+    // palindrome exps
+    int l = license(epochs, runs);
+    int s = str_exp(epochs, runs, n);
+    int i = arr_exp(epochs, runs, n);
     auto end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
     cout << "\n" << setprecision(10) << (end -(double) start) /(double) 1000 << "\n";
     return 0;
